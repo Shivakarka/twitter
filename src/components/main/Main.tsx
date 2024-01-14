46c44717b65a794eaa2fd498908363be6fa75da9
shivakarka <shivakarka@gmail.com>
2024-01-14 19:04:37 +0530

    Add page title and icons to Main component

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,10 +1,31 @@
-import "./main.css"
+import { TimelineIcon } from "../../utils/svgs";
+import "./main.css";
 const Main = () => {
-    return (
-        <div className={"main"}>
-            <h1>Main</h1>
+  return (
+    <div className={"main"}>
+      <div className="pageTitle">
+        <div className="pageTitleTop">
+          {" "}
+          <h2>Home</h2>
+          <div className="pageIcons">
+            <span>
+              <TimelineIcon />
+            </span>
+          </div>
         </div>
-    );
+        <div className="pageTitleBottom">
+          <div className="pageSection">
+            <p>For You</p>
+            <div className="blueBottom"></div>
+          </div>
+          <div className="pageSection">
+            <p>Following</p>
+            <div className="blueBottom"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
